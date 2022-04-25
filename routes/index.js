7a28bceef6008c2ae124f7f40009df63200c4be9
omar-faruk-tech <farukadekunle@gmail.com>
2022-04-25 07:01:59 +0100

    database hosted on cleardb

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Meditech backend application by team CODARCHS of Tiidelab Cohort 3' });
+  res.render('index', { title: 'Medarchs HMS Prototype' });
 });
 
 module.exports = router;
